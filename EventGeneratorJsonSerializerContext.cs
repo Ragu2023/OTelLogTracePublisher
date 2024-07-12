6870929bf9eefdcadc6c3546eaa486db9594aafe
Ragu Marimuthu <136855179+Ragu2023@users.noreply.github.com>
2024-07-12 16:33:18 -0700

    Add files via upload

---
+++ EventGeneratorJsonSerializerContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace EventGenerator
+{
+    [JsonSerializable(typeof(Dictionary<string, string>))]
+    [JsonSerializable(typeof(string[]))]
+    internal partial class EventGeneratorJsonSerializerContext : JsonSerializerContext
+    {
+    }
+}
